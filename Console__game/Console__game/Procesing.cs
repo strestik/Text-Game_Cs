3dce8d11948c5f1080385babc979dfc005e6344c
strestik <matejstrestik@gmail.com>
2025-12-24 03:18:24 +0100

    Add files via upload

---
+++ Console__game/Console__game/Procesing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Console__game
+{
+    internal class Processing
+    {
+        //public static void ApplyEffect (Character character, string effectName, int duration)
+        //{
+        //    if (character.effects.ContainsKey(effectName))
+        //    {
+        //        character.effects[effectName]["is"] = true;
+        //        character.effects[effectName]["duration"] = duration;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"Effect {effectName} does not exist on character {character.Name}.");
+        //    }
+        //}
+    }
+}
---
+++ Console__game/Console__game/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Character = Console__game.Character;
+
+namespace Console__game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Character hero = new Character("");
+                Console.WriteLine($"Character Created: {hero.Name}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+            }
+            
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
